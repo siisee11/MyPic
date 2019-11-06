73f38811e57d7826d44748a0ec31d0009d308de8
siisee11 <siisee111@gmail.com>
2019-11-07 01:16:15 +0900

    Add fonts

--- Mypic/Client/App/app/pages/downloadPic.js
+++ Mypic/Client/App/app/pages/downloadPic.js
@@ -13,6 +13,7 @@ import {
 
 import {Actions} from 'react-native-router-flux';
 import DownloadPicHeader from "../components/DownloadPicHeader"
+import * as Font from "expo-font";
 
 var { height, width } = Dimensions.get('window');
 
@@ -26,23 +27,32 @@ export default class DownloadPic extends Component {
                 tour_startedAt : null,
                 tour_images : [],
             },
+            fontLoaded: false,
         };
     }
 
     componentDidMount = async () => {
-      this.props.tour
-          .get()
-          .then(res => {
-            let data = res.data();
-            this.setState({
-                tour: {
-                    tour_name : data.tourName,
-                    tour_description : data.description,
-                    tour_startedAt : data.tourStartedAt,
-                    tour_images : data.images,
-                },
-            });
-      }).catch(error => console.log(error))
+        this.props.tour
+            .get()
+            .then(res => {
+                let data = res.data();
+                this.setState({
+                    tour: {
+                        tour_name : data.tourName,
+                        tour_description : data.description,
+                        tour_startedAt : data.tourStartedAt,
+                        tour_images : data.images,
+                    },
+                });
+            }).catch(error => console.log(error))
+
+        await Font.loadAsync({
+            'Gaegu-Regular': require('../../assets/fonts/Gaegu/Gaegu-Regular.ttf'),
+            'EastSeaDokdo-Regular': require('../../assets/fonts/East_Sea_Dokdo/EastSeaDokdo-Regular.ttf'),
+            'Nanum_pen_Script-Regular': require('../../assets/fonts/Nanum_Pen_Script/NanumPenScript-Regular.ttf'),
+            'Yeon_Sung-Regular': require('../../assets/fonts/Yeon_Sung/YeonSung-Regular.ttf'),
+        });
+        this.setState({ fontLoaded: true });
     };
 
     goBack() {
@@ -83,21 +93,21 @@ export default class DownloadPic extends Component {
         return(
             <SafeAreaView style={styles.container}>
                 <DownloadPicHeader title="Download Pictures" />
-                <Text style={{...styles.textInput, marginTop:20, fontSize: 25, fontWeight: "bold"}}>
+                <Text style={{...styles.textInput, marginTop:20, fontSize: 30, fontFamily: 'Yeon_Sung-Regular'}}>
                            {
                              this.state.tour.tour_name ?
                              this.state.tour.tour_name : null
                            }
                 </Text>
 
-                <Text style={{...styles.textInput, fontSize:15, }}>
+                <Text style={{...styles.textInput, fontSize:20,fontFamily: 'Nanum_pen_Script-Regular', }}>
                            {
                              this.state.tour.tour_startedAt ?
                              this.state.tour.tour_startedAt.toDate().toDateString() : null
                            }
                 </Text>
 
-                <Text style={{...styles.textInput, fontSize: 15,}}>
+                <Text style={{...styles.textInput, fontSize: 20,fontFamily: 'Yeon_Sung-Regular', }}>
                     {
                         this.state.tour.tour_description ?
                             this.state.tour.tour_description: null
