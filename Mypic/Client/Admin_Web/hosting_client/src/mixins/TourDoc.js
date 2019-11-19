48c101379aa65b2f90b88be6abdd0cb4ac595b75
BlindedShooter <gkadnflh@gmail.com>
2019-11-19 15:55:31 +0900

    Fix Vue data binding issue with tour document

--- Mypic/Client/Admin_Web/hosting_client/src/mixins/TourDoc.js
+++ Mypic/Client/Admin_Web/hosting_client/src/mixins/TourDoc.js
@@ -1,9 +1,18 @@
-import defaultTourDocument from '../utils/DefaultTourDocument'
-
 export default {
     data () {
       return {
-        tourDoc: defaultTourDocument
+        tourDoc: {
+          createdAt: new Date(),
+          editedAt: new Date(),
+          description: '',
+          images: [],
+          location: '',
+          participants: [],
+          thumbnail: '',
+          tourEndedAt: null,
+          tourName: '',
+          tourStartedAt: null
+        }
       }
     }
   }
