997187e03b1543cd5803b7f7f5b1f6c005a0afad
Jaeyoun <siisee111@gmail.com>
2019-11-21 03:53:55 -0500

    Modify download url

--- Mypic/Server/face-classification/download.sh
+++ Mypic/Server/face-classification/download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1DXVFH_3XTu4o4A8B43JQ9-wXCUqw4vCZ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1DXVFH_3XTu4o4A8B43JQ9-wXCUqw4vCZ" -O friend-dataset.tar.gz && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1VHoW2_tY-PlPaVfNh3r5TjHIAb6NQ2-N' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1VHoW2_tY-PlPaVfNh3r5TjHIAb6NQ2-N" -O ASC19-dataset.tar.gz && rm -rf /tmp/cookies.txt
 
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1PZ_6Zsy1Vb0s0JmjEmVd8FS99zoMCiN1' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1PZ_6Zsy1Vb0s0JmjEmVd8FS99zoMCiN1" -O facenet_keras.h5 && rm -rf /tmp/cookies.txt
